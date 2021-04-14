b98ae1c8e89841089ef0c39d7e7583db5eb376d2
Gian_Pereira(Jason Todd) <72985830+Jason21tod@users.noreply.github.com>
2021-04-14 19:50:13 -0300

    Mudando o arquivo todo
    Mudei o arquivo, por ter visto certos erros e coisas desnecessárias  haha

--- Meu_TestDroid.py
+++ Meu_TestDroid.py
@@ -1,223 +1,141 @@
-from time import sleep
-
-#Criei uma classe pra fazer testes unitários naquelas épocas de estudo de POO
-
-"""
-NOTA! Em todos os métodos que pedem um argumento 'func',
-ele se refere somente a uma função sem chamá-la, sendo que
-a existência desse argumento serve para um mapeamento de onde
-tais testes serão executados, com exceção apenas do método
-'run_function, que roda uma função que argumentos pré estabelecidos
-e todos os testes dentro dela.
-"""
-
-class TestDroidFunctional:
-    log_list = []
-    tests_concluded = 0
-    functions_mapped = 0
-    functions_runned = 0
-    tests_failed = 0
+class Mapper:
+    def __init__(self, name):
+        self.name = name
+        self._map = []
+        self._pre_map = []
     
-    @classmethod
-    def receive_msg(cls, msg: str) -> None:
-        """
-        Método que gera uma mensagem, adicionando-a ao resumo
-        de testes.
-        """
-        cls.log_list.append(msg)
+    def pre_map_func(self, func):
+        self._pre_map.append(f'pre mapped function >>> {func}')
+        return func
+    
+    def map_run(self, mapped):
+        self._map.append(f'\033[31;1m{"{"}\033[m\nrunning >>> {mapped} ')
+    
+    def map_inp(self, func, *inp):
+        for item in inp:
+            self._map.append(f'\033[33m input <<<\033[m {item}: {type(item)}\n \033[32m em {func}\033[m')
+        return inp
         
-    @classmethod
-    def map_functions(cls, func) -> None:
-        """
-        Mapeia funções, de modo a exibir as mesmas no resultado dos testes.
-        Este método não roda as funções.
-        """
-        cls.log_list.append(f'\033[32m>>> Mapeando {func}\033[m')
-        cls.functions_mapped += 1
+    def map_out(self, func, out):
+        self._map.append(f'\033[35m output >>> \033[m {out}: {type(out)}\n \033[32m em {func}\033[m \033[31;1m{"}"}\033[m')
+        return out
         
-    @classmethod
-    def emit_all_logs(cls) -> None:
-        """
-        Gera uma representação sistemática ( em ordem de execução ), de funções/métodos mapeados,
-        funções/métodos rodados, mensagens importantes, testes bem sucedidos e falhos, além de dar
-        a localização de tais funções ou métodos. 
-        """
-        print(34*'_')
-        print('EMITIR TODOS OS LOGS')
-        for msg in cls.log_list:
-            print(msg)
+    def show_map(self):
+        print ('='*36)
+        print(f'{"MAP":^36} ==⟩ \033[32;1m{self.name}\033[m')
+        print ()
+        for item in self._map:
+            print(f'\033[32m>>>Mapeado\n — {item}\033[m')
             print()
-            sleep(0.3)
             
-    @classmethod
-    def show_resum(cls) -> None:
-        """
-        Exibe um resumo de testes feitos, quantos destes são falhos e quantas funções do código foram mapeadas e rodadas.
-        """
-        print(f'Testes realizados >>> {cls.tests_concluded}')
-        print(f'Testes falhos >>> {cls.tests_failed}')
-        print(f'Funções/métodos mapeados >>> {cls.functions_mapped}')
-        print(f'funções/métodos rodados >>> {cls.functions_runned}')
-        print('TESTES CONCLUÍDOS...')
-     
-    @classmethod
-    def run_function(cls, func) -> None:
-        """
-        Recebe como argumento uma função sem chamá-la diretamente. A função/método chamado,
-        deverá possuir argumentos pré-estabelecidos, afim de não criar erros.
-        """
-        cls.log_list.append(f'>>>\033[32mIniciando... {func}\033[m')
-        cls.tests_concluded += 1
-        cls.functions_runned += 1
-        func()
-   
-    @classmethod
-    def testing_upper(cls, func, val: float, expected: float) -> None:
-        """
-        Verifica se o valor (val) é maior que o esperado(expected).
-        """
-        value = val
-        cls.tests_concluded += 1
-        if value >= expected:
-            cls.log_list.append(f'TESTE higher-> <"{value}" >= {expected}> em {func}>>> \033[32mOK\033[m')
-        else:
-            cls.log_list.append(f'TESTE higher-> <"{value}" >= {expected}> em {func}>>> \033[31mFAIL-> o valor é menor que o experado\033[m')
-            cls.tests_failed += 1
-
-    @classmethod
-    def testing_lower(cls, func, val= float, expected= float) -> None:
-        """
-        Verifica se o valor (val) é menor que o esperado(expected).
-        """
-        value = val
-        cls.tests_concluded += 1
-        if value <= expected:
-            cls.log_list.append(f'TESTE lower-> <"{value}" <= {expected}> em {func}>>> \033[32mOK\033[m')
-        else:
-            cls.log_list.append(f'TESTE lower-> <"{value}" <= {expected}> em {func}>>> \033[31mFAIL-> o valor é maior que o experado\033[m')
-            cls.tests_failed += 1
-            
-    @classmethod
-    def testing_equal(cls, func, val, expected) -> None:
-        """
-        Verifica se o valor (val) é igual que o esperado(expected).
-        """
-        value = val
-        cls.tests_concluded += 1
-        if value == expected:
-            cls.log_list.append(f'TESTE equal-> <"{value}" == {expected}> em {func}>>> \033[32mOK\033[m')
-        else:
-            cls.log_list.append(f'TESTE equal-> <"{value}" != {expected}> em {func}>>> \033[31mFAIL-> o diferente é maior que o experado\033[m')
-            cls.tests_failed += 1
-
-    @classmethod
-    def testing_type(cls, func, val, expected) -> None:
-        """
-        Verifica se o valor (val) é do mesmo tipo que o tipo de expected.
-        """
-        value = val
-        cls.tests_concluded += 1
-        if type(value) == type(expected):
-            cls.log_list.append(f'TESTE type -> <{type(value)} == {type(expected)}> em {func}>>> \033[32mOK\033[m')
-        else:
-            cls.log_list.append(f'TESTE type -> <{type(value)} == {type(expected)}> em {func}>>> \033[31mFail-> o Objeto não coincide com o esperado\033[m')
-            cls.tests_failed += 1
-            
-    @classmethod
-    def testing_in(cls, func, val, container) -> None:
-        """
-        Verifica se o valor (val) está num container específico(container).
-        """
-        value = val
-        cls.tests_concluded += 1
-        if value in container:
-            cls.log_list.append(f'TESTE in -> <{value} in {container}> em {func}>>> \033[32mOK\033[m')
-        else:
-            cls.log_list.append(f'TESTE in -> <{value} NOT IN {expected}> em {func}>>> \033[31mFail-> O item não esta no container\033[m')
-            cls.tests_failed += 1
-            
-    @classmethod
-    def testing_not_in(cls, func, val, container) -> None:
-        """
-        Verifica se o valor (val) não está num container específico(container).
-        """
-        value = val
-        cls.tests_concluded += 1
-        if value not in container:
-            cls.log_list.append(f'TESTE not in -> <{value} not in {container}> em {func}>>> \033[32mOK\033[m')
-        else:
-            cls.log_list.append(f'TESTE not in -> <{value} in {expected}> em {func}>>> \033[31mFail-> O item esta no container\033[m')
-            cls.tests_failed += 1
+    def show_pre_map(self):
+        print ('='*36)
+        print(f'{"PRE MAP":^36} ==⟩ \033[32;1m{self.name}\033[m')
+        print ()
+        for item in self._pre_map:
+            print(f'\033[32m>>>Pré Mapeado\n — {item}\033[m')
+            print()
 
 
-#As funções a baixo serão usadas na representação de testes. Simulando a utilização de TestDroidFuncional
-def is_number(num: float) -> bool:
-    try:
-        float(num)
-        return True
-    except:
-        pass
-    return False
 
-def value_assign(value_field= 'testando value assign') -> int:
-    """
-    Gera uma mensagem de input garantindo que o valor escrito seja um número.
-    retornando um número inteiro.
-    """
-    value = input(f'{value_field}: ').strip()
-    evaluation = is_number(value)
-    while evaluation != True:
-        value = input ('Insira um valor válido (int): ').strip()
-        evaluation = is_number(value)    
+class UnitTest:
+    def __init__ (self, name):
+        self.name = name
+        self.__tests_runned = 0
+        self.__tests_failed_logs = []
+        self.__tests_failed = 0
     
-    value = int(value)
-    TestDroidFunctional.testing_type(value_assign, value, 1)
-    return value
+    def is_equal(self, func_name, value, expected):
+        self.__tests_runned += 1
+        veredict = value == expected
+        if veredict == False:
+            self.create_fail_log(func_name, (f'VALOR DIFERENTE DO ESPERADO:\n {value} != {expected}'))
+        return value
 
+    def is_low(self, func_name, value, expected):
+        self.__tests_runned += 1
+        veredict = value < expected
+        if veredict == False:
+            self.create_fail_log(func_name, (f'VALOR MAIOR OU IGUAL AO ESPERADO:\n {value} >= {expected}'))
+        return value
+    
+    def is_higher(self, func_name, value, expected):
+        self.__tests_runned += 1
+        veredict = value > expected
+        if veredict == False:
+            self.create_fail_log(func_name, (f'VALOR MAIOR OU IGUAL AO ESPERADO:\n {value} != {expected}'))
+        return value
+        
+    def not_equal(self, func_name, value, expected):
+        self.__tests_runned += 1
+        veredict = value != expected
+        if veredict == False:
+            self.create_fail_log(func_name, (f'VALOR IGUAL AO ESPERADO:\n {value} == {expected}'))
+        return value
+        
+    def is_in(self, func_name, value, expected):
+        self.__tests_runned += 1
+        veredict = value in expected
+        if veredict == False:
+            self.create_fail_log(func_name, (f'VALOR NÃO ESTÁ NO ARRAY:\n {value} not in {expected}'))
+        return value
+        
+    def not_in(self, func_name, value, expected):
+        self.__tests_runned += 1
+        veredict = value not in expected
+        if veredict == False:
+            self.create_fail_log(func_name, (f'VALOR ESTÁ NO ARRAY:\n {value} in {expected}'))
+        return value
+        
+    def is_type(self, func_name, value, expected):
+        self.__tests_runned += 1
+        veredict = type(value) == expected
+        if veredict == False:
+            self.create_fail_log(func_name, (f'TIPO DIFERENTE DO ESPERADO:\n {type(value)} != {expected}'))
+        return value
+
+    def not_type(self, func_name, value, expected):
+        self.__tests_runned += 1
+        veredict = type(value) != expected
+        if veredict == False:
+            self.create_fail_log(func_name, (f'TIPO IGUAL AO ESPERADO:\n {type(value)} == {expected}'))
+        return value
+
+    def create_fail_log(self, func_name, msg):
+        self.__tests_failed_logs.append(f'\033[31mErro em {func_name}\033[m \n>>> {msg}\n')
+        self.__tests_failed += 1
+        
+    def show_fails(self):
+        for test in self.__tests_failed_logs:
+            print(test)
+        
+    def run_mocked_function(self, func):
+        try:
+            func()
+        except:
+            raise Exception(f'\nA função {func} não possui mocks \n')
+            
+    def show_resum(self):
+        print(f'\033[4m{self.name:_^36}\033[m')
+        print(f'\nTestes rodados: {self.__tests_runned}')
+        print(f'Testes falhos: {self.__tests_failed}')
+        print('='*36)
+        self.show_fails()
+        
 
 if __name__ == '__main__':
-    #CASO VOCÊ RODE NESSA PASTA:
-    """
-    O arquivo sera executado, mostrando essa área.
-    """
-    print('TESTES')
-
-    """    
-    na função abaixo, são posicionados por padrão dois
-    números 1 caso você não dê os argumentos pra função.
-    """
-    def soma_dois(n1 = 1, n2 = 1):
-        #soma dois números
-        soma = n1 + n2
-
-        #testando se soma é maior q 1
-        TestDroidFunctional.testing_upper(soma_dois, soma, 1)
-        
-        #testando se a soma é menor que 3
-        TestDroidFunctional.testing_lower(soma_dois, soma, 3)
-        
-        #testando se soma é igual a 2
-        TestDroidFunctional.testing_equal(soma_dois, soma, 2)
-        
-        #testando se o tipo é o mesmo de 0
-        TestDroidFunctional.testing_type(soma_dois, soma, 0)
-        
-        return soma
-
-    """
-    com essa função, você ve todos os logs do testdroid,
-    mapeando a função.
-    """
+    tdu = UnitTest('testador teste')
+    mp = Mapper('mapeador teste')
     
-    TestDroidFunctional.run_function(value_assign)
+    @mp.pre_map_func
+    def soma(n1= 3, n2= 3):
+        mp.map_inp(soma, n1, n2)
+        value = n1+n2
+        tdu.not_equal(soma, value, 5)
+        tdu.is_equal(soma, value, 2)
+        return mp.map_out(soma, value)
     
-    #rode essa função
-    TestDroidFunctional.run_function(soma_dois)
-    
-    #diz pra mim todos os seus logs
-    TestDroidFunctional.emit_all_logs()
-    
-    #diz pra mim um resumo
-    TestDroidFunctional.show_resum()
-    
+    soma()
+    tdu.show_resum()
+    mp.show_map()
     
